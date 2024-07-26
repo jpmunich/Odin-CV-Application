f1d1f3874a27711bad9586d297f4fb2ea64756fe
jpmunich <parkercoyle8@gmail.com>
2024-07-26 19:39:39 -0400

    Construct static site

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import Resume from "./Resume";
+import Form from "./Form";
+import "../styles/App.css";
+
+function App() {
+  return (
+    <div className="ui-container">
+      <Form />
+      <Resume />
+    </div>
+  )
+}
+
+export default App
